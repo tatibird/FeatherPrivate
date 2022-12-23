c37c18f8d7d55fb604557ccacb483b945c35f1b2
EZEZEZEZZE <104246252+EZEZEZEZZE@users.noreply.github.com>
2022-12-23 01:34:47 -0600

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
-        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/Ln3242/FeatherPrivatet/main/mainscript.lua'))()")
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/EZEZEZEZZE/FeatherPrivatet/main/mainscript.lua'))()")
     end
 end)
 
@@ -17,30 +17,30 @@ if placeid == 6872274481 or placeid == 8560631822 or placeid == 8444591321 then
     if betterisfile("Feather-Client/Modules/Bedwars.lua") then
         loadstring(readfile("Feather-Client/Modules/Bedwars.lua"))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ln3242/FeatherPrivate/main/Modules/Bedwars.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EZEZEZEZZE/FeatherPrivate/main/Modules/Bedwars.lua"))()
     end
 elseif placeid == 6872265039 then
     if betterisfile("Feather-Client/Modules/6872265039.lua") then
         loadstring(readfile("CometV2/Modules/6872265039.lua"))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ln3242/FeatherPrivate/main/Modules/6872265039.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EZEZEZEZZE/FeatherPrivate/main/Modules/6872265039.lua"))()
     end
 elseif placeid == 4282985734 then
     if betterisfile("Feather-Client/Modules/4282985734.lua") then
         loadstring(readfile("Feather-Client/Modules/4282985734.lua"))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ln3242/FeatherPrivate/main/Modules/Key.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EZEZEZEZZE/FeatherPrivate/main/Modules/Key.lua"))()
     end
 elseif placeid == 11640391378 then
     if betterisfile("Feather-Client/Modules/11640391378.lua") then
         loadstring(readfile("CometV2/Modules/11640391378.lua"))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ln3242/FeatherPrivate/main/Modules/11640391378.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EZEZEZEZZE/FeatherPrivate/main/Modules/11640391378.lua"))()
     end
 else
     if betterisfile("Feather-Client/Modules/Universal.lua") then
         loadstring(readfile("Feather-Client/Modules/Universal.lua"))()
     else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Ln3242/FeatherPrivate/main/Modules/Universal.lua"))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/EZEZEZEZZE/FeatherPrivate/main/Modules/Universal.lua"))()
     end
 end
